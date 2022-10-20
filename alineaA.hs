75f04418100a8b8d03101baf9e938da2ccba8777
ines-08 <insagaspar08@gmail.com>
2022-10-20 18:56:28 +0100

    add function normalization that eliminates zeros and incognits (^0)

--- alineaA.hs
+++ alineaA.hs
@@ -1,21 +1,29 @@
 import AddPoly
 
 --TODO
-orderMonom :: Monom -> Monom
-orderMonom x = x
+--orderMonom :: Monom -> Monom
+--orderMonom x = x
 
-orderPolynom :: [Monom] -> [Monom]
-orderPolynom [] = []
-orderPolynom (x:xs) = orderMonom x : orderPolynom xs
+--orderPolynom :: [Monom] -> [Monom]
+--orderPolynom [] = []
+--orderPolynom (x:xs) = orderMonom x : orderPolynom xs
 
 elimZerosPolynom :: [Monom] -> [Monom]
-elimZerosPolynom (x:xs) | fst x == 0 = elimZeros xs
-                        | otherwise = x : elimZeros xs
+elimZerosPolynom [] = []
+elimZerosPolynom (x:xs) | fst x == 0 = elimZerosPolynom xs
+                        | otherwise = x : elimZerosPolynom xs
+
+x = [(3, [("x", 2), ("y", 0)]), (6, [("t", 4)])]
+
+verifyIncognitExist :: Incognit -> Incognit
+verifyIncognitExist [] = []
+verifyIncognitExist (x:xs) = if (snd x == 0) then verifyIncognitExist xs else x : verifyIncognitExist xs
 
 elimIncognitPolynom :: [Monom] -> [Monom]
-elimIncognitPolynom (x:xs) | snd snd x == 0 = (x, [("", 0)]) : elimIncognitPolynom xs
-                           | otherwise = x : elimZero
+elimIncognitPolynom [] = []
+elimIncognitPolynom (x:xs) = (fst x, verifyIncognitExist (snd x)) : elimIncognitPolynom xs
+
 
 --TODO
-normalizePolynom :: [Monom] -> [Monom]
-normalizePolynom x = x
+--normalizePolynom :: [Monom] -> [Monom]
+--normalizePolynom x = x
