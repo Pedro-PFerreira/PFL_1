9067c2aec508d8aed2ea4f771aab864858e0e936
Pedro Ferreira <up202004986@edu.fe.up.pt>
2022-10-20 15:23:00 +0100

    finished multiplication, parsing normalisation left. We're so close!

--- alineaC.hs
+++ alineaC.hs
@@ -7,11 +7,16 @@ equalIncognit a b = (fst a * fst b, sumExp (snd a) (snd b))
 sumExp :: Incognit -> Incognit -> Incognit
 sumExp a [] = a
 sumExp [] a = a
+sumExp a b = if fst (head a) == fst (head b) then (fst (head a), snd (head a) + snd (head b)) : sumExp (tail a) (tail b)
+ else head a : sumExp (tail a) b
+
+{- 
 sumExp (a:as) (b:bs) = if (fst a == fst b) then [(fst a, snd a + snd b)] ++ sumExp as bs else
     [(fst a, snd a), (fst b, snd b)] ++ sumExp as bs
+-}
 
 t = [("x", 2), ("y", 3)]
-w = [("y", 3)]
+w = [("x", 4),("y", 3), ("z", 4)]
 
 x = [(3, [("x", 2), ("y", 3)]), (6, [("t", 2)])]
 
@@ -24,15 +29,13 @@ z = [(12, [("x", 4), ("y", 3)]),
 (24, [("t", 2), ("x", 2)]),
 (30, [("t", 2), ("z", 3)]),
 (30, [("t", 2), ("y", 3)]) 
-]
 -}
 
+
 {-- se forem iguais, multiplicam-se os coeficiente, mantem-se a incógnita (primeiro
-elemento do tuplo) e somam-se os expoentes (segundo elemento do tuplo)
 --}
 
 {-- se forem diferentes, multiplicam-se os coeficientes e adicionam-se a incógnita
-e os expoentes à lista de incógnitas de a
 --}
 
 multiPolynom :: [Monom] -> [Monom] -> [Monom]
