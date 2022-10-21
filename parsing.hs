e84cf69236f546a2c001583213b8aabe7ae62bad
Pedro Ferreira <up202004986@edu.fe.up.pt>
2022-10-21 12:28:01 +0100

    Extracted coeficients

--- parsing.hs
+++ parsing.hs
@@ -1,36 +1,13 @@
 import Data.List.Split
 
 import Data.Char
-input = "3*x^2 + 2*y + 5*z + y - 7*y^2 + 4*x*y^2"
+k = "3*x^2 + 2*y + 5*z + y - 7*y^2 + 4*x*y^2"
 
-vars = splitOn " " input
+elimSpaces :: String -> String -> [String]
+elimSpaces list target = splitOn target list
 
-lista = filter (/= " ") vars
+z = "42*z^2"
 
-removeFirst::[a] -> [a]
-removeFirst [] = []
-removeFirst [x] = []
-removeFirst (x:xs) = xs
-
-addSignal::[String] -> [[String]]
-addSignal [] = []
-addSignal (x:xs)
-                | x == "+" = [x, head xs] : addSignal (removeFirst xs)
-                | x == "-" = [x, head xs] : addSignal (removeFirst xs)
-                | otherwise = ["+", x] : addSignal xs
-
-res = addSignal lista
-
-multiplyrem :: [[String]] -> [[String]]
-multiplyrem = map (filter (/= "*"))
-
-
-example = [["+", "3", "x", "2"], ["-", "4", "x", "2"], ["+", "3", "x", "1"]]
-
-{-
--- Ultimate solution for splitting
-splitOnAnyOf :: Eq a => [[a]] -> [a] -> [[a]]
-splitOnAnyOf ds xs = foldl (\ys d -> ys >>= splitOn d) [xs] ds
-
-miau = [x | x <-splitOnAnyOf [" "] "2 * x + y -z", x /= ""]
--}
+extractCoef:: String -> String
+extractCoef [] = []
+extractCoef x = if head x /= '*' then head x : extractCoef (tail x) else []
