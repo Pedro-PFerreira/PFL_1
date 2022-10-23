543d2facb280f394fbac171cda0eaaa3c051170b
Pedro Ferreira <up202004986@edu.fe.up.pt>
2022-10-23 02:59:04 +0100

    Finished revered. Only main and test left

--- reverse.hs
+++ reverse.hs
@@ -14,7 +14,7 @@ type Incognit = [(String, Integer)]
 type Monom = (Coef, Incognit)
 -}
 
-k = [(3,[("x",2)]),(2,[("y",1)]),(5,[("z",1)]),(1,[("y",1)]),(-7,[("y",2)]),(4,[("x",1),("y",2)])]
+k = [(-3,[("x",2)]),(2,[("y",1)]),(5,[("z",1)]),(1,[("y",1)]),(-7,[("y",2)]),(4,[("x",1),("y",2)])]
 
 l = (4, [("x", 3), ("y", -3), ("t", 5)])
 
@@ -28,16 +28,27 @@ joinIncognit x = [fst y ++ "^" ++ toString(snd y) | y <- x]
 strIncognit :: [String] -> String
 strIncognit [] = []
 strIncognit (x:xs) | head x == '-' = "(" ++ x ++ ")" ++ strIncognit xs
+                   | null xs= x
                    | otherwise = x ++ "*" ++ strIncognit xs
 
 joinCoef :: (Integer, String) -> String
-joinCoef x = toStringCoef (fst x) ++ "*" ++ snd x 
+joinCoef x = toStringCoef (fst x) ++ "*" ++ snd x
 
 toStringCoef :: Integer -> String
-toStringCoef s | s < 0 = " -" ++ show (s * (-1))
-           | otherwise = " +" ++ show s
+toStringCoef s | s < 0 = " - " ++ show (s * (-1))
+           | otherwise = " + " ++ show s
 
 g = joinCoef (fst l, strIncognit (joinIncognit (snd l)))
 
-toStringMonom :: (Integer, Incognit) -> String
+toStringMonom :: Monom -> String
 toStringMonom x = joinCoef (fst x, strIncognit (joinIncognit (snd x)))
+
+toStringPolynom :: [Monom] -> String
+toStringPolynom [] = []
+toStringPolynom x = toStringMonom (head x) ++ toStringPolynom (tail x)
+
+removeExtras :: [Monom] -> String
+removeExtras x = dropWhile (== ' ') (toStringPolynom x)
+
+finalForm :: [Monom] -> String
+finalForm x = if fst (head x) > 0 then drop 1 (dropWhile (== '+') (removeExtras x)) else dropWhile (== '+') (removeExtras x)
